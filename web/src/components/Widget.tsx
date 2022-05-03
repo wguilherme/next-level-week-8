4c3e47e8e1d4cf1ac236ed2c833b427067b2f3bf
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 10:29:35 -0300

    feat: add accessibility popover to widget component + headlessui lib

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,33 +1,28 @@
+import { Popover } from '@headlessui/react'
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
 
 export function Widget() {
 
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
 
   return (
-    <div className="absolute bottom-5 right-5">
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Working</Popover.Panel>
 
-      {isWidgetOpen && <p>Working</p>}
-
-      <button onClick={toggleWidgetVisibility} className="flex items-center
+      <Popover.Button className="flex items-center
       bg-brand-500 rounded-full px-3 h-12 text-white
       group
       ">
+
         <ChatTeardropDots className="w-6 h-6 " />
-        <span className="
+        <span
+          className="
         max-w-0 overflow-hidden group-hover:max-w-xs
         transition-all duration-500 ease-linear
         ">
-          {/* <span className="pl-2"></span> */}
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
 
   )
 }
