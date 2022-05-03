963529d40a325e5e7a8672e318d5a03d2395f390
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 12:21:13 -0300

    feat: add component flow, select options, show content, show form, form UI, chore add tailwind forms and tailwind scrollbar lib

---
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,64 @@
+import { ArrowLeft } from 'phosphor-react';
+import { CloseButton } from '../../CloseButton';
+import { FeedbackType, feedbackTypes } from './../';
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested
+}: FeedbackContentStepProps) {
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <>
+      <header>
+
+        <button
+          type="button"
+          className="
+          top-5 left-5 absolute
+          text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+
+        </button>
+
+        <span className="text-xl leading-6">
+          <img
+            className="w-6 h-6"
+            src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+
+        <form className="my-4 w-full">
+
+          <textarea
+            className="
+            min-w-[304px] w-full min-h-[112px]
+            text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600
+            bg-transparent rounded-md focus:border-brand-500 
+            focus:ring-brand-500 focus:ring-1 focus:outline-none
+            scrollbar scrollbar-thumb-zinc-700
+            scrollbar-track-transparent
+            scrollbar-thin
+            resize-none"
+            placeholder="Conte com detalhes o que está acontecendo"
+          />
+
+
+        </form>
+
+      </div>
+    </>
+  )
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,5 +9,9 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+
+  ],
 }
