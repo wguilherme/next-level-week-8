2812bda25882242870f42763874a021b98c9d71b
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 12:35:49 -0300

    feat: extend tailwind class style + add camera button

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,8 +7,12 @@ module.exports = {
           300: '#996DFF',
           500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: "4px"
       }
     },
+
   },
   plugins: [
     require('@tailwindcss/forms'),
